b77227c8b832f9076ea96d96c52a9002c2cfc17d
agustin mebar <agustin.mebar@tsoftlatam.com>
2024-06-19 12:48:13 -0300

    clase 06

---
+++ src/clean-code/06-classes-b.ts
@@ -0,0 +1,89 @@
+(() => {
+  // No aplicando el principio de responsabilidad única
+
+  type Gender = "M" | "F";
+
+  interface PersonProps {
+    name: string;
+    gender: Gender;
+    birthdate: Date;
+  }
+
+  class Person {
+    public name: string;
+    public gender: Gender;
+    public birthdate: Date;
+
+    constructor({ name, gender, birthdate }: PersonProps) {
+      this.name = name;
+      this.gender = gender;
+      this.birthdate = birthdate;
+    }
+  }
+
+  interface UserProps {
+    email: string;
+    role: string;
+    name: string;
+    gender: Gender;
+    birthdate: Date;
+  }
+
+  class User extends Person {
+    public email: string;
+    public role: string;
+    public lastAccess: Date;
+
+    constructor({ email, role, name, gender, birthdate }: UserProps) {
+      super({ name, gender, birthdate });
+      this.lastAccess = new Date();
+      this.email = email;
+      this.role = role;
+    }
+
+    checkCredentials() {
+      return true;
+    }
+  }
+
+  interface UserSettingProps {
+    email: string;
+    role: string;
+    name: string;
+    gender: Gender;
+    birthdate: Date;
+    workingDirectory: string;
+    lastOpenFolder: string;
+  }
+
+  class UserSettings extends User {
+    public lastOpenFolder: string;
+    public workingDirectory: string;
+
+    constructor({
+      email,
+      role,
+      name,
+      gender,
+      birthdate,
+      workingDirectory,
+      lastOpenFolder
+    }: UserSettingProps) {
+      super({ email, role, name, gender, birthdate });
+      this.workingDirectory = workingDirectory;
+      this.lastOpenFolder = lastOpenFolder;
+    }
+  }
+
+  const userSettings = new UserSettings({
+    workingDirectory: "/usr/home",
+    lastOpenFolder: "/home",
+    email: "fernando@google.com",
+    role: "Admin",
+    name: "Fernando",
+    gender: "M",
+    birthdate: new Date("1985-10-21")}
+  );
+
+  console.log({ userSettings });
+})();
