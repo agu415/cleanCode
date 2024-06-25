a9ea4b004a4469144857a3d6b44ae33d0d014c50
agustin mebar <agustin.mebar@tsoftlatam.com>
2024-06-24 22:35:44 -0300

    tareas finalizadas Code Smells

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import './style.css'
 //import './clean-code/04-homework'
 //import './clean-code/05-dry'
 //import './clean-code/06-classes-a'
-// import './clean-code/06-classes-b'
-// import './clean-code/06-classes-c'
-import './clean-code/07-tarea'
+//import './clean-code/06-classes-b'
+//import './clean-code/06-classes-c'
+//import './clean-code/07-tarea'
+//import './code-smells/02-high-coupling'
+import './code-smells/02-low-coupling'
+
 
 const app = document.querySelector<HTMLDivElement>('#app')!
 
